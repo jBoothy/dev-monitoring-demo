458c0fb3339cc838a83d166140df88e7ccd036ab
John Booth <jhunter.booth@gmail.com>
2022-04-28 12:32:59 -0600

    added rollbar

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8df491279a664750a1e3ce9ce979c501',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4545
 
 app.listen(port, ()=>console.log(`Server is running on port: ${port}`))
