a3112ff8d70f023913d9653543f3b2453c90eb6f
John Booth <jhunter.booth@gmail.com>
2022-04-28 12:53:05 -0600

    narrowing bug

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-// // include and initialize the rollbar library with your access token
-// var Rollbar = require('rollbar')
-// var rollbar = new Rollbar({
-//   accessToken: '8df491279a664750a1e3ce9ce979c501',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8df491279a664750a1e3ce9ce979c501',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
-// // record a generic message and send it to Rollbar
-// rollbar.log('Hello world!')
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 // let students = []
 
